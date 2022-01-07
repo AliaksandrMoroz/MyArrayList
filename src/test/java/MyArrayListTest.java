a1983bd9817b107a03ddffd315f8fff35d5ea3a0
AliaksandrMoroz <89323365+AliaksandrMoroz@users.noreply.github.com>
2022-01-07 09:23:14 +0300

    add testMethod

--- src/test/java/MyArrayListTest.java
+++ src/test/java/MyArrayListTest.java
@@ -3,7 +3,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.omg.CORBA.portable.ApplicationException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -57,42 +60,120 @@ class MyArrayListTest {
     }
 
     @Test
-    void testRemove() {
+    void testRemoveShouldReturnFalseForValueNotFound(){
+        MyArrayList<String> testList = new MyArrayList(5);
+        testList.add("test");
+        testList.add("test");
+        testList.add("test");
+        assertFalse(testList.remove("not found"));
     }
 
     @Test
-    void get() {
+    void testRemoveInValueShouldReturnTrue(){
+        MyArrayList<String> testList = new MyArrayList(5);
+        testList.add("test");
+        testList.add("alex");
+        testList.add("stop");
+        assertTrue(testList.remove("alex"));
     }
 
     @Test
-    void indexOf() {
+    void testGetShouldReturnElement(){
+        assertEquals(32,beforeMyArrayList.get(2));
     }
 
     @Test
-    void testAdd() {
+    void testIndexOfShouldReturnNegativeOneForValueNotFound(){
+        assertEquals(-1,beforeMyArrayList.indexOf(1000));
     }
 
     @Test
-    void set() {
+    void testIndexOfShouldReturnIndexForCorrectValue(){
+        assertEquals(2,beforeMyArrayList.indexOf(32));
     }
 
     @Test
-    void contains() {
+    void testAddIndexAndValueShouldReturnTrue(){
+        assertTrue(beforeMyArrayList.add(3,100));
+    }
+    @Test
+    void testAddIndexAndValueShouldInstallValueForIndex(){
+        beforeMyArrayList.add(3,100);
+        assertEquals(100,beforeMyArrayList.get(3));
     }
 
     @Test
-    void isEmpty() {
+    void testSetShouldReturnOldValue(){
+        assertEquals(32,beforeMyArrayList.set(2,100));
     }
 
     @Test
-    void clear() {
+    void testSetShouldInstallValueForIndex(){
+        beforeMyArrayList.set(2,100);
+        assertEquals(100,beforeMyArrayList.get(2));
     }
 
     @Test
-    void sortByToString() {
+    void testContainsShouldReturnFalseIfValueNotFound(){
+        assertFalse(beforeMyArrayList.contains(100));
     }
 
     @Test
-    void bubbleSortMethod() {
+    void testContainsShouldReturnFalseIfValueNull(){
+        assertFalse(beforeMyArrayList.contains(null));
     }
+
+    @Test
+    void testContainsShouldReturnTrueIfCorrectValue(){
+        assertTrue(beforeMyArrayList.contains(32));
+    }
+
+    @Test
+    void testIsEmptyShouldReturnTrueIfListEmpty(){
+        MyArrayList<String> testList = new MyArrayList(5);
+        assertTrue(testList.isEmpty());
+    }
+
+    @Test
+    void testIsEmptyShouldReturnFalseIfListNotEmpty(){
+        assertFalse(beforeMyArrayList.isEmpty());
+    }
+    @Test
+    void testClearShouldDeleteAllElements(){
+        beforeMyArrayList.clear();
+        assertEquals(0,beforeMyArrayList.size());
+    }
+
+    @Test
+    void testSortByToStringShouldSortedList(){
+        List<String> testList = new ArrayList<>();
+        testList.add("test");
+        testList.add("Alex");
+        testList.add("stop");
+        Collections.sort(testList);
+        MyArrayList<String> testMyList = new MyArrayList(3);
+        testMyList.add("test");
+        testMyList.add("Alex");
+        testMyList.add("stop");
+        testMyList.sortByToString();
+        assertArrayEquals(testList.toArray(),testMyList.getArrayValue());
+    }
+
+    @Test
+    void testBubbleSortMethodShouldSortedIntegerClass(){
+        List<Integer> testList = new ArrayList<>();
+        testList.add(53);
+        testList.add(4);
+        testList.add(32);
+        testList.add(2);
+        testList.add(1);
+        testList.add(56);
+        Collections.sort(testList);
+        beforeMyArrayList.bubbleSortMethod(beforeMyArrayList);
+
+        assertArrayEquals(testList.toArray(), beforeMyArrayList.getArrayValue());
+
+
+    }
+
 }
